0f6550f0ae7cc36b17f856cee687b2973b7b6741
boyunovaaa <olga.boyunova@gmai.com>
2022-07-09 16:19:54 +0300

    commit

--- src/main/java/com/example/add/HelloController.java
+++ src/main/java/com/example/add/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.add;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController{
 
@@ -29,8 +34,20 @@ public class HelloController{
 
     @FXML
     void initialize() {
-        checkButton.setOnAction(actionEvent -> {
-            System.out.println("Вы нажали кнопку 'Войти'");
+        newUserButton.setOnAction(actionEvent -> {
+            newUserButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("registration.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent source = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(source));
+            stage.showAndWait();
         });
     }
 }
--- src/main/java/com/example/add/RegistrationController.java
+++ src/main/java/com/example/add/RegistrationController.java
@@ -7,7 +7,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
-
 public class RegistrationController {
 
     @FXML
